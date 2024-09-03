94e978e5237c891ce6d77d72bdaab6e0891b47aa
Pradeepasri2907 <pradeepasri2004@gmail.com>
2024-09-03 20:30:34 +0530

    hello

---
+++ src/com/tnsif/day3/AbstractionDemo.java
@@ -0,0 +1,24 @@
+package com.tnsif.day3;
+
+abstract class value1
+{
+	abstract void one();
+}
+
+class value2 extends value1
+{
+	public void one () {
+		System.out.println("Abstract method in abstract class");
+	}
+}
+
+
+public class AbstractionDemo {
+
+	public static void main(String[] args) {
+		value2 val = new value2();
+		val.one();
+	}
+}
+
+
